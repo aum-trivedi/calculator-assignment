61165c2ce3487cb15492d2013580d19c693271d9
aum-trivedi <aum@solulab.co>
2019-01-16 17:41:25 +0530

    Calculator functions

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+/**
+
+Author:Aum Trivedi
+
+**/
+function checkKeyPress(){
+ 		
+        var seven = document.getElementById('seven').value;
+        document.getElementById('display_text').value += seven;
+       
+		 
+     	var eight = document.getElementById('eight').value;
+        document.getElementById('display_text').value += eight;
+
+         
+}
