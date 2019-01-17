6cf36cc461d1b895fdc3c52526438678580de043
aum-trivedi <aum@solulab.co>
2019-01-17 18:05:15 +0530

    single operand function add

--- js/calculator.js
+++ js/calculator.js
@@ -7,7 +7,10 @@ var firstnumber;
 var secondnumber;
 var result;
 var operations;
-//This function for get Button key  stroke 
+//This function for get Button key  stroke
+
+
+
 function checkKeyPress(command){
  	if(command=="one" || document.getElementById('display_text').value == "0" || document.getElementById('display_text').value == result)
             {
@@ -62,7 +65,7 @@ function checkKeyPress(command){
             }
     if(command=="point")
             {
-               document.getElementById('display_text').value = ".";
+               document.getElementById('display_text').value += ".";
                 document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
 }
@@ -79,43 +82,63 @@ function add()
 // Substraction between numbers
 function sub()
 {
-    operation = "*";  
-    firstnumber=parseInt(document.getElementById('display_text').value);
-      document.getElementById('history_data').value = document.getElementById('display_text').value;
-    console.log(firstnumber);
-    document.getElementById('display_text').value= firstnumber + operation; 
+   document.getElementById("display_text").value += "-";
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
     
 }
 
 // Multiply between numbers
 function multiply()
 {
-     operation = "*";  
-    firstnumber=parseInt(document.getElementById('display_text').value);
-     document.getElementById('history_data').value = document.getElementById('display_text').value;
-    console.log(firstnumber);
-    document.getElementById('display_text').value= firstnumber + operation; 
+     document.getElementById("display_text").value += "*";
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
 }
 
 // Square with single operands
 function square()
 {
-     
-    firstnumber=parseInt(document.getElementById('display_text').value);
-    console.log(firstnumber);
-     
-    document.getElementById('display_text').value= square(firstnumber); 
+     var c=document.getElementById("display_text").value
+    document.getElementById("display_text").value = Math.pow(c, 2);
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
 }
 
+function qube()
+{
+     var c=document.getElementById("display_text").value
+    document.getElementById("display_text").value = Math.pow(c, 3);
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
+}
+
+function sqrt()
+{
+     var c=document.getElementById("display_text").value
+    document.getElementById("display_text").value = Math.sqrt(c);
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
+}
 // Square between two operands
 function divide()
 {
-     operation = "/";  
-    firstnumber=parseInt(document.getElementById('display_text').value);
-    console.log(firstnumber);
-    document.getElementById('display_text').value= firstnumber + operation;  
+     document.getElementById("display_text").value += "/";
+   document.getElementById('history_data').value = document.getElementById('display_text').value; 
 }
 
+function fact()
+{
+     var c=document.getElementById("display_text").value
+    document.getElementById("display_text").value = factorial(c);
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
+}
+
+function exponent()
+{
+   
+    var x="^";
+   firstnumber=document.getElementById("display_text").value;
+   var a=Array();
+   a.push(document.getElementById("display_text").value);
+   console.log(a);
+   document.getElementById('history_data').value = firstnumber+x+secondnumber;
+}
 function equalsto(){
  
  document.getElementById("display_text").value = eval(document.getElementById("display_text").value);
@@ -126,6 +149,7 @@ function removeData(){
      
     document.getElementById('display_text').value =""; 
 }
+
 function deleteChar()
 {
     document.getElementById('history_data').value = "";
@@ -134,3 +158,8 @@ function deleteChar()
  
  
 
+function factorial(x) {
+  if (x < 0) return;
+  if (x === 0) return 1;
+  return x * factorial(x - 1);
+}
