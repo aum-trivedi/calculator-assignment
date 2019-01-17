f3faf6e991bb5fed34718f35e1b1c1ebb3c4ce22
aum-trivedi <aum@solulab.co>
2019-01-17 15:19:04 +0530

    Add History Script

--- js/calculator.js
+++ js/calculator.js
@@ -3,57 +3,67 @@
 Author:Aum Trivedi
 
 **/
-var firstnumber;  
-var secondnumber;  
-var result;  
-var operations;  
-
+var firstnumber;
+var secondnumber;
+var result;
+var operations;
 //This function for get Button key  stroke 
 function checkKeyPress(command){
- 	if(command=="one")
+ 	if(command=="one" || document.getElementById('display_text').value == "0" || document.getElementById('display_text').value == result)
             {
                document.getElementById('display_text').value += "1";
+               document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
-    if(command=="two")
+    if(command=="two" )
             {
                document.getElementById('display_text').value += "2";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="three")
             {
                document.getElementById('display_text').value += "3";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="four")
             {
                document.getElementById('display_text').value += "4";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="five")
             {
                document.getElementById('display_text').value += "5";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="six")
             {
                document.getElementById('display_text').value += "6";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     
     if(command=="seven")
             {
                document.getElementById('display_text').value += "7";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="eight")
             {
                document.getElementById('display_text').value += "8";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
      if(command=="nine")
             {
                document.getElementById('display_text').value += "9";
+                 document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="zero")
             {
                document.getElementById('display_text').value += "0";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
     if(command=="point")
             {
                document.getElementById('display_text').value = ".";
+                document.getElementById('history_data').value = document.getElementById('display_text').value;
             }
 }
  
@@ -61,11 +71,9 @@ function checkKeyPress(command){
 // Addition between numbers
 function add()
 {
-    operation = "+";  
-    firstnumber=parseInt(document.getElementById('display_text').value);
-    console.log(firstnumber);
-    document.getElementById('display_text').value= firstnumber + operation; 
-    
+     
+   document.getElementById("display_text").value += "+";
+   document.getElementById('history_data').value = document.getElementById('display_text').value;
 }
 
 // Substraction between numbers
@@ -73,6 +81,7 @@ function sub()
 {
     operation = "*";  
     firstnumber=parseInt(document.getElementById('display_text').value);
+      document.getElementById('history_data').value = document.getElementById('display_text').value;
     console.log(firstnumber);
     document.getElementById('display_text').value= firstnumber + operation; 
     
@@ -83,6 +92,7 @@ function multiply()
 {
      operation = "*";  
     firstnumber=parseInt(document.getElementById('display_text').value);
+     document.getElementById('history_data').value = document.getElementById('display_text').value;
     console.log(firstnumber);
     document.getElementById('display_text').value= firstnumber + operation; 
 }
@@ -106,28 +116,19 @@ function divide()
     document.getElementById('display_text').value= firstnumber + operation;  
 }
 
-//Press equal give total result
-function equalsto(){  
-   
- secondnumber = parseInt(document.getElementById('display_text').value);  
-console.log(secondnumber); 
- if (operation == "+")  
- {  
-  result = firstnumber + secondnumber;  
-    
- }  
-if (operation == "*")  
- {  
-  result = firstnumber * secondnumber;  
-    
- }   
-   
- document.getElementById('display_text').value = firstnumber + operation + secondnumber + " = " + result.toString();
-     return;
+function equalsto(){
+ 
+ document.getElementById("display_text").value = eval(document.getElementById("display_text").value);
+  
 }
 
 function removeData(){
      
+    document.getElementById('display_text').value =""; 
+}
+function deleteChar()
+{
+    document.getElementById('history_data').value = "";
     document.getElementById('display_text').value =""; 
 }
  
